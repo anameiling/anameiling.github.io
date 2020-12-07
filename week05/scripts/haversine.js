e66955dcf44e29692e31638cae30a1e6e9b52c34
anameiling <anathasia17lee@gmail.com>
2020-12-07 21:16:41 +0800

    2116

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -20,7 +20,7 @@ if(typeof Number.prototype.toRad == "undefined"){
 }
 
 function roundVal(val){
-    let dec 2;
+    let dec = 2;
     let result = Math.round(val * Math.pow(10,dec)) / Math.pow(10,dec);
     return result;
 }
