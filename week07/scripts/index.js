4ed94980c147c2699a9ad498b645ef709fc66918
anameiling <anathasia17lee@gmail.com>
2020-12-14 21:34:01 +0800

    2133

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -37,3 +37,14 @@ let app4 = new Vue({
         ]
     }
 });
+
+let app5 = new Vue({
+    el:"#app5",
+    data: {
+        messageL "Hakim Ariff"
+    },
+    methods: {
+        reverseMessage: function(){
+            this.message = this.message.split("").reverse().join("");        }
+    }
+})
