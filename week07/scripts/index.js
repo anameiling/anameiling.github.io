e56cb13e3ec6d25d6bd371339a0e0b093d3b18e6
anameiling <anathasia17lee@gmail.com>
2020-12-14 20:47:58 +0800

    2047

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -8,6 +8,6 @@ let app= new Vue({
 let app2 = new Vue({
     el:"#app2",
     data: {
-        message: "You loaded this page on " + new Date(). toLocaleString()
+        message: "You loaded this page on " + new Date().toLocaleString()
     }
 })
