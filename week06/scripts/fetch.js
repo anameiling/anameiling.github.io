378c68706161491be41aca62b3c30496c0a25de8
anameiling <anathasia17lee@gmail.com>
2020-12-15 20:30:25 +0800

    2030

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -1,7 +1,9 @@
-let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/bookingApp (projectDeepWeek06)/bookings';
-fetch(url)
-.then((response) => response.json())
-.then(json => {
-  // Do something with the data
-  console.log(json.bookings);
-});
+function GetBooking() {
+    let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/bookingApp (projectDeepWeek06)/bookings';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+        console.log(json.bookings);
+        });
+}
