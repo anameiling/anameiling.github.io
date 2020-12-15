a5a968442469bda4e84716f286c276bcac62c85b
anameiling <anathasia17lee@gmail.com>
2020-12-15 20:29:27 +0800

    2029

---
+++ week06/scripts/fetch.js
@@ -0,0 +1,7 @@
+let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/bookingApp (projectDeepWeek06)/bookings';
+fetch(url)
+.then((response) => response.json())
+.then(json => {
+  // Do something with the data
+  console.log(json.bookings);
+});
