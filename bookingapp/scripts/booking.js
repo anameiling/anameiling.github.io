8d329921d96b962d7f73033c3a168ed0a633f530
anameiling <anathasia17lee@gmail.com>
2020-12-30 13:22:19 +0800

    1322

---
+++ bookingapp/scripts/booking.js
@@ -0,0 +1,35 @@
+function BookNow(guestName, guestEmail, guestPax, guestRemarks) {
+    let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/studioSessionBookingApp (project)/sheet1';
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            phone:guestPhone,
+            pax: guestPax,
+            datetime: guestDateTime
+    }
+  }
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers:{
+        "Content-Type": "application/json"
+    }
+  })
+    .then((response) => response.json())
+    .then(json => {
+        alert(json.booking.name + " added!");
+    });
+}
+
+window.addEventListener("load", function() {
+    document.getElementById("bookNow").addEventListener("click", function() {
+        let userName = document.getElementById("userName").value;
+        let userEmail = document.getElementById("userEmail").value;
+        let userPhone = document.getElementById("userPhone").value;
+        let userPax = document.getElementById("userPax").value;
+        let userDateTime = document.getElementById("userDateTime").value;
+        
+        BookNow(userName, userEmail, userPhone, userPax, userDateTime);
+    });
+});
