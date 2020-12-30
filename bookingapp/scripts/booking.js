7474539103f5168bcb1f12b839e863991b383cf8
anameiling <anathasia17lee@gmail.com>
2020-12-30 13:35:04 +0800

    1334

--- bookingapp/scripts/booking.js
+++ bookingapp/scripts/booking.js
@@ -1,5 +1,5 @@
 function BookNow(guestName, guestEmail, guestPax, guestRemarks) {
-    let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/studioSessionBookingApp (project)/sheet1';
+    let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/studioSessionBookingApp (project)/bookings';
     let body = {
         booking: {
             name: guestName,
