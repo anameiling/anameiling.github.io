0e93dce98ce14c77bc516821004503e0546e5980
anameiling <anathasia17lee@gmail.com>
2021-01-01 19:47:14 +0800

    1947

--- bookingapp/scripts/booking.js
+++ bookingapp/scripts/booking.js
@@ -28,7 +28,7 @@ window.addEventListener("load", function() {
         let userEmail = document.getElementById("userEmail").value;
         let userPhone = document.getElementById("userPhone").value;
         let userPax = document.getElementById("userPax").value;
-        let userDateTime = document.getElementById("userDateTime").value;
+        let userDateTime = document.getElementById("userDateTime").value = "2021-01-01T11:42:13.510";
         
         BookNow(userName, userEmail, userPhone, userPax, userDateTime);
     });
