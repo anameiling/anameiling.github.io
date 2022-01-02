b87b84d6f581dff54a4ba133bea63de046b4c86e
anameiling <anathasia17lee@gmail.com>
2022-01-03 03:20:38 +0800

    0320

--- bookingapp/scripts/booking.js
+++ bookingapp/scripts/booking.js
@@ -1,4 +1,4 @@
-function BookNow(guestName, guestEmail, guestPhone, guestPax, guestSlot, guestInstrument, guestMessage) {
+function BookNow(guestName, guestEmail, guestPhone, guestPax, guestSlot, /**guestInstrument,**/ guestMessage) {
     let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/studioBookingApp/booking';
     let body = {
         booking: {
@@ -7,7 +7,7 @@ function BookNow(guestName, guestEmail, guestPhone, guestPax, guestSlot, guestIn
             phone: guestPhone,
             pax: guestPax,
             slot: guestSlot,
-            instrument: guestInstrument,
+            // instrument: guestInstrument,
             message: guestMessage
     }
   }
@@ -31,10 +31,10 @@ window.addEventListener("load", function() {
         let userPhone = document.getElementById("userPhone").value;
         let userPax = document.getElementById("userPax").value;
         let userSlot = document.getElementById("userSlot").value;
-        let userInstrument = document.getElementById("userInstrument").value;
+        // let userInstrument = document.getElementById("userInstrument").value;
         let userMessage = document.getElementById("userMessage").value;
         
-        BookNow(userName, userEmail, userPhone, userPax, userSlot, userInstrument, userMessage);
+        BookNow(userName, userEmail, userPhone, userPax, userSlot, /**userInstrument,**/ userMessage);
     });
 });
 
