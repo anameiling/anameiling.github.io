91d37cc71164da667a54c246aaf5874d2cc6453d
anameiling <anathasia17lee@gmail.com>
2022-01-03 05:21:20 +0800

    0521

---
+++ bookingapp-original/scripts-original/booking-original.js
@@ -0,0 +1,35 @@
+function BookNow(guestName, guestEmail, guestPhone, guestPax, guestSlot) {
+    let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/studioBookingApp/booking';
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            phone: guestPhone,
+            pax: guestPax,
+            slot: guestSlot
+    }
+  }
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers:{
+        "Content-Type": "application/json"
+    }
+  })
+    .then((response) => response.json())
+    .then(json => {
+        alert(json.booking.name + " added!");
+    });
+}
+
+window.addEventListener("load", function() {
+    document.getElementById("bookNow").addEventListener("click", function() {
+        let userName = document.getElementById("userName").value;
+        let userEmail = document.getElementById("userEmail").value;
+        let userPhone = document.getElementById("userPhone").value;
+        let userPax = document.getElementById("userPax").value;
+        let userSlot = document.getElementById("userSlot").value;
+        
+        BookNow(userName, userEmail, userPhone, userPax, userSlot);
+    });
+});
---
+++ bookingapp-original/scripts-original/view-original.js
@@ -0,0 +1,63 @@
+function GetBooking() {
+    let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/studioBookingApp/booking';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            //console.log(json.booking);
+            let viewList = document.getElementById("viewList");
+            let viewIds = [];
+
+            //clear the table rows
+            for(let k = viewList.rows.length - 1; k > 0; k--){
+                viewList.deleteRow(k);
+            }
+
+            for(let i = 0; i < json.booking.length; i++){
+                let gName = json.booking[i].name;
+                let gEmail = json.booking[i].email;
+                let gPhone = json.booking[i].phone;
+                let gPax = json.booking[i].pax;
+                let gId = json.booking[i].id;
+                let gSlot= json.booking[i].slot;
+                let buttonId = "delete" + gId;
+
+                let row = viewList.insertRow(viewList.rows.length);
+                row.insertCell(0).innerHTML = gId;
+                row.insertCell(1).innerHTML = gName;
+                row.insertCell(2).innerHTML = gEmail;
+                row.insertCell(3).innerHTML = gPhone;
+                row.insertCell(4).innerHTML = gPax;
+                row.insertCell(5).innerHTML = gSlot;
+                row.insertCell(6).innerHTML = "<button id='" + buttonId + "'class='btn btn-danger'>Delete</button>";
+
+                viewIds.push(buttonId);
+
+            }
+
+            for(let j =0; j < viewIds.length; j++){
+                let el = document.getElementById(viewIds[j]);
+                el.addEventListener("click", function(){
+                    let theId = viewIds[j].replace("delete","");
+                    DeleteBooking(theId);
+                });
+            }
+        });
+}
+
+window.addEventListener("load", function() {
+    document.getElementById("refreshList").addEventListener("click", function() {
+        GetBooking();
+    });
+})
+
+function DeleteBooking(id){
+    let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/studioBookingApp/booking/' + id;
+    fetch(url, {
+        method: 'DELETE',
+    })
+        .then((response) => {
+            GetBooking();
+        });
+
+}
