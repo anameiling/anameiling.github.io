6e76b5f9b1578cdbc2360ee4f854d701f140bc62
anameiling <anathasia17lee@gmail.com>
2022-01-03 12:37:36 +0800

    1237

--- bookingapp/scripts/login.js
+++ bookingapp/scripts/login.js
@@ -1,5 +1,5 @@
 function Login(loginName, loginPassword){
-    let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/studioBookingSessionSystem/loginn';
+    let url = 'https://api.sheety.co/2287ba5e9c156f97e011e5cdc03a23cc/studioBookingSessionSystem/login';
     let body = {
         login: {
             name: loginName,
