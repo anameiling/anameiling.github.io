456c368d942c6f600887dda4ec4dd161b6ecdefa
anameiling <anathasia17lee@gmail.com>
2022-01-05 02:53:31 +0800

    0253

--- bookingapp/scripts/register.js
+++ bookingapp/scripts/register.js
@@ -10,9 +10,6 @@ function Register(username, useremail, userpassword){
     fetch(url, {
       method: 'POST',
       body: JSON.stringify(body),
-      headers:{
-        "Content-Type": "application/json"
-      }
     })
     .then((response) => response.json())
     .then(json => {
